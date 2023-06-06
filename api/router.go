ca856ec36673d0f37b27755fff1f920694a553f4
Alex Acebo <44811138+aacebo@users.noreply.github.com>
2023-06-06 11:57:32 -0700

    Update router.go

--- api/router.go
+++ api/router.go
@@ -1,14 +1,21 @@
 package api
 
 import (
+	"net/http"
 	"vayeate/api/clients"
 	"vayeate/node"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 )
 
 func NewRouter(n *node.Node) *chi.Mux {
 	r := chi.NewRouter()
 	clients.NewRouter(r, n)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		render.JSON(w, r, n)
+	})
+
 	return r
 }
